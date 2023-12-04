fa5908b0ac22555452dd1354afd3c067acc13612
LogicPy <waynekenneyjr@gmail.com>
2023-12-03 21:11:14 -0500

    Including Controllers and views directory for project. (Still unfinished...)
    Still need to connect all to local database...

---
+++ Review Application/Review Application/Controllers/ReviewsController.cs
@@ -0,0 +1,45 @@
+﻿using System.Web.Mvc;
+using Review_Application.Models; // Replace with your actual namespace
+using System.Linq;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Mvc;
+
+public class ReviewsController : Controller
+{
+    private MovieReviewContext db = new MovieReviewContext();
+
+    // GET: Reviews/Create
+    public ActionResult Create()
+    {
+        // If you have a list of movies, use it to populate a dropdown
+        ViewBag.MovieId = new SelectList(db.Movies, "MovieId", "Title");
+        return View();
+    }
+
+    // POST: Reviews/Create
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public ActionResult Create([Bind(Include = "ReviewId,ReviewerName,Content,Rating,MovieId")] Review review)
+    {
+        if (ModelState.IsValid)
+        {
+            db.Reviews.Add(review);
+            db.SaveChanges();
+            return RedirectToAction("Index", "Movies"); // Redirect to movies list after adding review
+        }
+
+        // Re-populate the MovieId dropdown in case of error
+        ViewBag.MovieId = new SelectList(db.Movies, "MovieId", "Title", review.MovieId);
+        return View(review);
+    }
+
+    // Dispose the context when the controller is disposed
+    protected override void Dispose(bool disposing)
+    {
+        if (disposing)
+        {
+            db.Dispose();
+        }
+        base.Dispose(disposing);
+    }
+}
