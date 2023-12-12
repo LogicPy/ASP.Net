7238568b9180764c2897c0dab9982176eef9e26f
LogicPy <waynekenneyjr@gmail.com>
2023-12-11 21:32:57 -0500

    Web Browser With jQuery Compatibility

---
+++ Web Browser Application/Form1.Designer.cs
@@ -0,0 +1,92 @@
+﻿namespace Web_Browser_Application
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.urlTextBox = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.webControl1 = new EO.WinForm.WebControl();
+            this.webView2 = new EO.WebBrowser.WebView();
+            this.SuspendLayout();
+            // 
+            // urlTextBox
+            // 
+            this.urlTextBox.Location = new System.Drawing.Point(13, 13);
+            this.urlTextBox.Name = "urlTextBox";
+            this.urlTextBox.Size = new System.Drawing.Size(337, 20);
+            this.urlTextBox.TabIndex = 1;
+            this.urlTextBox.Text = "http://wayne.cool/";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(356, 11);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Go! ";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // webControl1
+            // 
+            this.webControl1.BackColor = System.Drawing.Color.White;
+            this.webControl1.Location = new System.Drawing.Point(13, 39);
+            this.webControl1.Name = "webControl1";
+            this.webControl1.Size = new System.Drawing.Size(1448, 711);
+            this.webControl1.TabIndex = 3;
+            this.webControl1.Text = "webControl1";
+            this.webControl1.WebView = this.webView2;
+            // 
+            // webView2
+            // 
+            this.webView2.Url = "http://wayne.cool/";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1473, 849);
+            this.Controls.Add(this.webControl1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.urlTextBox);
+            this.Name = "Form1";
+            this.Text = "JQuery compatible WebBrowser - For Personal Use";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox urlTextBox;
+        private System.Windows.Forms.Button button1;
+        private EO.WinForm.WebControl webControl1;
+        private EO.WebBrowser.WebView webView2;
+    }
+}
+
---
+++ Web Browser Application/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Web_Browser_Application;
+using EO.WebBrowser;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.Button;
+using Microsoft.Web.WebView2.Core;
+using Microsoft.Web.WebView2.WinForms;
+
+namespace Web_Browser_Application
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            webView2 = new WebView();
+
+            //webView1.JSInitCode = string.Format("window.setTimeout(function() {{ {0} }}, 5);", Properties.Resources.jquery);
+
+            // Load a URL
+            webView2.LoadUrl("https://example.com");
+
+           
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string url = urlTextBox.Text;
+            if (!string.IsNullOrEmpty(url))
+            {
+                if (!url.StartsWith("http://") && !url.StartsWith("https://"))
+                {
+                    url = "http://" + url;
+                }
+                webView2.LoadUrl((url));
+            }
+        }
+    }
+}
